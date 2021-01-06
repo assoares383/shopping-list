217f5b909e5f0dd21cfe110ccfddff7cd6f2143b
assoares383 <alexandressoares383@gmail.com>
2021-01-06 17:50:34 -0300

    action and reducer form

--- src/components/CreateList/ListItemFooter/index.js
+++ src/components/CreateList/ListItemFooter/index.js
@@ -1,10 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { Creators as FormActions } from "../../../store/actions/Form";
 
 const ListItemFooter = (props) => (
   <div className="list-card-footer">
     <div className="list-card-footer-actions">
-      <FontAwesomeIcon icon={faPen} color="#00b0ff" size="1x" />
+      <FontAwesomeIcon
+        onClick={() => props.startUpdate(props.item)}
+        icon={faPen}
+        color="#00b0ff"
+        size="1x"
+      />
       <FontAwesomeIcon
         onClick={() => props.deleteProduct(props.item.id)}
         icon={faTrash}
@@ -16,4 +25,7 @@ const ListItemFooter = (props) => (
   </div>
 );
 
-export default ListItemFooter;
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(FormActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(ListItemFooter);
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import list from "./List";
+import form from "./Form";
 
 export default combineReducers({
   list,
+  form,
 });
